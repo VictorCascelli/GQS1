99069b9b38cdb3c8d31131b3b9ca65c4ff216d9a
Victor Almeida Cascelli Schwenck <victorcascellischwenck@gmail.com>
2024-06-26 19:49:54 -0300

    Recreating EsferaP4.java to Esfera.java p2

---
+++ EsferaP2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EsferaP2 {
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+
+        double raioEsfera = 0.0;
+
+        System.out.print("Raio: ");
+        raioEsfera = console.nextDouble();
+
+        double comprimentoEsfera = 2 * Math.PI * raioEsfera;
+        System.out.println("Comprimento: " + comprimentoEsfera);
+
+        double areaEsfera = Math.PI * Math.pow(raioEsfera, 2);
+        System.out.println("area: " + areaEsfera);
+
+        double volumeEsfera = 4/3 * Math.PI * Math.pow(raioEsfera, 3);
+        System.out.println("Volume:" + volumeEsfera);
+}
