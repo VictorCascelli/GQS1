c3a430905c2fbb69330a1015db1a40bf0d4420e7
Victor Almeida Cascelli Schwenck <victorcascellischwenck@gmail.com>
2024-06-26 19:41:42 -0300

    Renaming to P2

--- FahrenheitsP2.java
+++ FahrenheitsP2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Fahrenheits {
+public class FahrenheitsP2 {
     public static void main(String[] args) {
 
         Scanner console = new Scanner(System.in);
