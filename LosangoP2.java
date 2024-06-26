15266c876539614873472ba696d3e4ea4a89defb
Victor Almeida Cascelli Schwenck <victorcascellischwenck@gmail.com>
2024-06-26 19:47:16 -0300

    Recreating Losango.java P1 to Losando.java P2

---
+++ LosangoP2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Losango {
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+
+        float diagonalMaior = 0;
+        float diagonalMenor = 0;
+
+        System.out.print("Digite a diagonal maior: ");
+        diagonalMaior = console.nextFloat();
+
+        System.out.print("Digite a diagonal menor: ");
+        diagonalMenor = console.nextFloat();
+
+        float areaLosango = (diagonalMaior * diagonalMenor)/2;
+
+        System.out.println("A área do losango é de " + areaLosango);
+        
+    }
+}
